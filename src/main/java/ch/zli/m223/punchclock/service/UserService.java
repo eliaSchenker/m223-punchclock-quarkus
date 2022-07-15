0962fc1cecf96b9ae29f3958d7f6af4379fab289
EliaSchenker <schenkere@bzz.ch>
2022-07-15 15:21:26 +0200

    Update UserService.java
    Added new UserService

--- src/main/java/ch/zli/m223/punchclock/service/UserService.java
+++ src/main/java/ch/zli/m223/punchclock/service/UserService.java
@@ -1,5 +1,7 @@
 package ch.zli.m223.punchclock.service;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -21,8 +23,35 @@ public class UserService {
     }
 
     @Transactional
-    public void createNewUser(User user) {
+    public User createNewUser(User user) {
         user.setPassword(BcryptUtil.bcryptHash(user.getPassword())); //Hash the users password
         entityManager.persist(user);
+        return user;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<User> findAll() {
+        var query = entityManager.createQuery("FROM User");
+        return query.getResultList();
+    }
+
+    @Transactional
+    public User updateUser(User user) {
+        //If the password was not set by the update, use the password of the existing user
+        if(user.getPassword() == "" || user.getPassword() == null) {
+            User toEditUser = entityManager.find(User.class, user.getId());
+            user.setPassword(toEditUser.getPassword());
+        }else {
+            user.setPassword(BcryptUtil.bcryptHash(user.getPassword())); //Hash the users new password
+        }
+        entityManager.merge(user);
+        return user;
+    }
+
+    @Transactional
+    public User deleteUser(long id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+        return user;
     }
 }
